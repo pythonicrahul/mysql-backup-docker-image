edf0fbef109e206a70aa293478912577a60ff99b
stpldevops <stpl.devops@gmail.com>
2023-09-28 00:26:42 +0530

    partially working

--- main.py
+++ main.py
@@ -14,9 +14,43 @@ db_name = os.environ.get('DB_NAME')
 aws_access_key = os.environ.get('AWS_ACCESS_KEY')
 aws_secret_key = os.environ.get('AWS_SECRET_KEY')
 s3_bucket_name = os.environ.get('S3_BUCKET_NAME')
+restore_backup = os.environ.get('RESTORE_BACKUP')  # Get the restore flag
+backup_file_name = os.environ.get('BACKUP_FILE_NAME')  # Get the backup file name
 
-# Maximum number of attempts to check MySQL availability
-max_attempts = 10  # Adjust as needed
+
+def database_exists(database_name):
+    try:
+        subprocess.run(
+            f"mysql -h {db_host} -P {db_port} -u {db_user} -p{db_password} -e 'USE {database_name}'",
+            shell=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def restore_database(database_name, sql_backup_file):
+    subprocess.run(
+        f"mysql -h {db_host} -P {db_port} -u {db_user} -p{db_password} {database_name} < {sql_backup_file}",
+        shell=True,
+        check=True,
+    )
+
+if restore_backup.lower() == 'true':
+    sql_backup_file = f"{backup_file_name}.sql"
+
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key,
+                      aws_secret_access_key=aws_secret_key)
+    s3.download_file(s3_bucket_name, sql_backup_file, sql_backup_file)
+
+    if not database_exists(db_name):
+        restore_database(db_name, sql_backup_file)
+        print(f"{db_name} restored successfully.")
+
+max_attempts = 10 
 attempt = 0
 
 while attempt < max_attempts:
@@ -31,21 +65,18 @@ while attempt < max_attempts:
             stderr=subprocess.PIPE,
         )
 
-        # If the connection succeeds, create a backup
-        backup_filename = f"backup_{project_name}_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.sql"
+        # Create a backup
+        backup_filename = f"{backup_file_name}_{project_name}_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.sql"
         mysql_dump_command = f"mysqldump -h {db_host} -P {db_port} -u {db_user} -p{db_password} {db_name} > {backup_filename}"
         subprocess.run(mysql_dump_command, shell=True, check=True)
 
-        # Initialize the S3 client (specify the region if necessary)
-        s3 = boto3.client('s3', aws_access_key_id=aws_access_key,
-                          aws_secret_access_key=aws_secret_key)
-
         # Upload the backup file to S3
-        with open(backup_filename, 'rb') as backup_file:
-            s3.upload_fileobj(backup_file, s3_bucket_name, backup_filename)
-
+        s3.upload_file(backup_filename, s3_bucket_name, backup_filename)
         print(f"Backup successfully uploaded to S3: {backup_filename}")
-        break  # Exit the loop if backup is successful
+
+        os.remove(backup_filename)
+
+        break  
 
     except subprocess.CalledProcessError:
         # MySQL is not yet available, wait and retry
